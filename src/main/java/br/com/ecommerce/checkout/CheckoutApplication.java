238f615342828c46803242822e5efa621114b9e7
Caio Souza <caio.7s@outlook.com>
2021-05-01 19:24:38 -0300

    feat: Inicializando Projeto Spring

---
+++ src/main/java/br/com/ecommerce/checkout/CheckoutApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecommerce/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
