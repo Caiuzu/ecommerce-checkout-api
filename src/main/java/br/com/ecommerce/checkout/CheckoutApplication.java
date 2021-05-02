7f7a29d2bafb549eebde58b4c63acecc91ab8991
Caio Souza <caio.7s@outlook.com>
2021-05-02 01:55:33 -0300

    feat: Habilitando Swagger na Classe Main

--- src/main/java/br/com/ecommerce/checkout/CheckoutApplication.java
+++ src/main/java/br/com/ecommerce/checkout/CheckoutApplication.java
@@ -2,8 +2,10 @@ package br.com.ecommerce.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CheckoutApplication {
 
 	public static void main(String[] args) {
