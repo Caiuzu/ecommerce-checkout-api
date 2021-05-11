c2105c8f48128eda620096b4e52abd71110c3c9e
Caio Souza <caio.7s@outlook.com>
2021-05-11 20:36:55 -0300

    feat: Criando repositório CheckoutRepository

---
+++ src/main/java/br/com/ecommerce/checkout/repository/CheckoutRepository.java
@@ -0,0 +1,14 @@
+package br.com.ecommerce.checkout.repository;
+
+import br.com.ecommerce.checkout.entity.CheckoutEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<CheckoutEntity, Long> {
+
+    Optional<CheckoutEntity> findByCode(String code);
+}
+
