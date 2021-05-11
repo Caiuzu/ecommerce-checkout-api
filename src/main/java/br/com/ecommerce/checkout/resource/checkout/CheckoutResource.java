a71c1af3fb14d5801b4a72c9cef9931495825791
Caio Souza <caio.7s@outlook.com>
2021-05-11 20:33:41 -0300

    feat: Criando Controller CheckoutResource

---
+++ src/main/java/br/com/ecommerce/checkout/resource/checkout/CheckoutResource.java
@@ -0,0 +1,33 @@
+package br.com.ecommerce.checkout.resource.checkout;
+
+import br.com.ecommerce.checkout.entity.CheckoutEntity;
+import br.com.ecommerce.checkout.service.CheckoutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/checkout")
+@RequiredArgsConstructor
+public class CheckoutResource {
+
+    private final CheckoutService checkoutService;
+
+    @PostMapping("/")
+    public ResponseEntity<CheckoutResponse> create(@RequestBody CheckoutRequest checkoutRequest) {
+        try {
+            final CheckoutEntity checkoutEntity = checkoutService.create(checkoutRequest)
+                    .orElseThrow(IllegalArgumentException::new);
+            final CheckoutResponse checkoutResponse = CheckoutResponse.builder()
+                    .code(checkoutEntity.getCode())
+                    .build();
+            return ResponseEntity.status(HttpStatus.CREATED).body(checkoutResponse);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
