b6d3f3aefdf985b63290cb28e00846b602ab8e28
Caio Souza <caio.7s@outlook.com>
2021-05-11 21:31:04 -0300

    feat: Criando entidade ShippingEntity

---
+++ src/main/java/br/com/ecommerce/checkout/entity/ShippingEntity.java
@@ -0,0 +1,40 @@
+package br.com.ecommerce.checkout.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShippingEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String address;
+
+    @Column
+    private String complement;
+
+    @Column
+    private String country;
+
+    @Column
+    private String state;
+
+    @Column
+    private String cep;
+
+    @OneToOne(mappedBy = "shipping")
+    private CheckoutEntity checkout;
+}
