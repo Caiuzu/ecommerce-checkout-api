58a258e1529423462fe8ec0243618ecb13afd729
Caio Souza <caio.7s@outlook.com>
2021-05-11 21:30:43 -0300

    feat: Criando Entidade de CheckoutItemEntity

---
+++ src/main/java/br/com/ecommerce/checkout/entity/CheckoutItemEntity.java
@@ -0,0 +1,28 @@
+package br.com.ecommerce.checkout.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckoutItemEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String product;
+
+    @ManyToOne
+    private CheckoutEntity checkout;
+}
